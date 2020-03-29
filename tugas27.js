047a50ae2d46b17b6142ea0bf14f7c8a4e5ad223
Simon Fredy Sinaga <simonfredysinaga@gmail.com>
2020-03-29 18:53:37 +0700

    Tugas 27 JS

---
+++ tugas27.js
@@ -0,0 +1,21 @@
+function tugasDuaPuluhTujuh() {
+    // Tugas 1
+    var a = "No";
+    var b = "rumah saya";
+    var c = 30;
+    var d = "A";
+
+    var gabung = a + " " + b + " " + String(c) + " " + d;
+    console.log(gabung);
+    console.log(gabung);
+
+    //Tugas 2
+    var e = "Jalan pangeran Tirtayasa No";
+    var f = 70;
+
+    var gabung2 = e + " " + String(f);
+    console.log(gabung2);
+    
+    
+}
+tugasDuaPuluhTujuh();
